81391bdb434bce4fb88883f8b93720cad7956968
unknown <reddyreva11@gmail.com>
2024-12-31 13:33:12 +0530

    initial commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "A. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Markup Language",
+            "B. High Text Markup Language",
+            "C. Hyper Tabular Markup Language",
+            "D. Hyperlink and Text Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which property is used to change the background color in CSS?",
+        answer: "C. background-color",
+        options: [
+            "A. color",
+            "B. bgcolor",
+            "C. background-color",
+            "D. background"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which JavaScript method is used to select an HTML element by ID?",
+        answer: "B. getElementById",
+        options: [
+            "A. getElementByClassName",
+            "B. getElementById",
+            "C. querySelector",
+            "D. getElementByTagName"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheets",
+        options: [
+            "A. Cascading Style Sheets",
+            "B. Creative Style Sheets",
+            "C. Computer Style Sheets",
+            "D. Colorful Style Sheets"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which JavaScript function is used to print content in the console?",
+        answer: "A. console.log()",
+        options: [
+            "A. console.log()",
+            "B. print()",
+            "C. log.console()",
+            "D. display()"
+        ]
+    }
+];
